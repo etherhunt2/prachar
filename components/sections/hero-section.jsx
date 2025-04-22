3d8421582bb5ce9935a6b4b0d7604e0dfff3f9e6
etherhunt2 <etherhunt2@gmail.com>
2025-04-22 16:52:55 +0530

    Hero Fixed

--- components/sections/hero-section.jsx
+++ components/sections/hero-section.jsx
@@ -277,7 +277,7 @@ export default function HeroSection() {
                   viewBox="0 0 24 24"
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
-                  className="text-mustard"
+                  className="text-white z-20 opacity-95"
                 >
                   <path
                     d="M12 5V19M12 19L19 12M12 19L5 12"
